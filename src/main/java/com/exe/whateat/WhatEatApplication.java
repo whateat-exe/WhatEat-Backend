81680062f69312fcc21ca12d99c21d2e5af3e353
Vincent Dao <vincentdaomain@gmail.com>
2024-05-03 22:43:20 +0700

    init

---
+++ src/main/java/com/exe/whateat/WhatEatApplication.java
@@ -0,0 +1,12 @@
+package com.exe.whateat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatEatApplication.class, args);
+    }
+}
