49b648dd874f2d81bc44be6aa86a3984ddd3fcc9
Vincent Dao <vincentdaomain@gmail.com>
2024-05-13 11:03:41 +0700

    Add @CrossOrigin to fix CORS.

--- src/main/java/com/exe/whateat/application/common/AbstractController.java
+++ src/main/java/com/exe/whateat/application/common/AbstractController.java
@@ -1,7 +1,9 @@
 package com.exe.whateat.application.common;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@CrossOrigin
 @RequestMapping("${whateat.api.path}")
 public abstract class AbstractController {
 
