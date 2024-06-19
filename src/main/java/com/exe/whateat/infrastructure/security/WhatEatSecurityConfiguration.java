1f3f538f44695930f5f763bd96df8fc1775692c4
forevpurity <auvv310103@gmail.com>
2024-06-19 17:59:25 +0700

    fix review security config

--- src/main/java/com/exe/whateat/infrastructure/security/WhatEatSecurityConfiguration.java
+++ src/main/java/com/exe/whateat/infrastructure/security/WhatEatSecurityConfiguration.java
@@ -162,9 +162,10 @@ public class WhatEatSecurityConfiguration {
 
     private void handleReviewApi(HttpSecurity http) throws Exception {
         final String dishReviewPath = "/dishes/{id}/reviews";
-        final String reviewPath = "/reviews";
+        final String reviewPath = "/reviews/**";
         http.authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.GET, resolvePath(dishReviewPath))
-                        .hasAuthority(AccountRole.USER.name()))
+                        .hasAnyAuthority(
+                                AccountRole.USER.name(), AccountRole.RESTAURANT.name(), AccountRole.MANAGER.name(), AccountRole.ADMIN.name()))
                 .authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.POST, resolvePath(dishReviewPath))
                         .hasAuthority(AccountRole.USER.name()))
                 .authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.DELETE, resolvePath(reviewPath))
@@ -172,7 +173,8 @@ public class WhatEatSecurityConfiguration {
                 .authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.PATCH, resolvePath(reviewPath))
                         .hasAuthority(AccountRole.USER.name()))
                 .authorizeHttpRequests(c -> c.requestMatchers(HttpMethod.GET, resolvePath(reviewPath))
-                        .hasAuthority(AccountRole.USER.name()));
+                        .hasAnyAuthority(
+                                AccountRole.USER.name(), AccountRole.RESTAURANT.name(), AccountRole.MANAGER.name(), AccountRole.ADMIN.name()));
     }
 
     private String resolvePath(String path) {
