9e15af7531a6a7295b07bfe89804e7798f7daab6
forevpurity <auvv310103@gmail.com>
2024-06-21 09:14:43 +0700

    get dishes by food

--- src/main/java/com/exe/whateat/application/dish/GetDishesByFood.java
+++ src/main/java/com/exe/whateat/application/dish/GetDishesByFood.java
@@ -4,16 +4,20 @@ import com.blazebit.persistence.CriteriaBuilderFactory;
 import com.blazebit.persistence.querydsl.BlazeJPAQuery;
 import com.exe.whateat.application.common.AbstractController;
 import com.exe.whateat.application.common.request.PaginationRequest;
+import com.exe.whateat.application.dish.entity.DishFilter;
 import com.exe.whateat.application.dish.mapper.DishMapper;
 import com.exe.whateat.application.dish.response.DishesResponse;
 import com.exe.whateat.application.exception.WhatEatErrorCode;
 import com.exe.whateat.application.exception.WhatEatException;
+import com.exe.whateat.entity.common.ActiveStatus;
 import com.exe.whateat.entity.common.WhatEatId;
 import com.exe.whateat.entity.food.Dish;
-import com.exe.whateat.entity.food.FoodTag;
 import com.exe.whateat.entity.food.QDish;
+import com.exe.whateat.entity.random.QRating;
 import com.exe.whateat.infrastructure.exception.WhatEatErrorResponse;
 import com.exe.whateat.infrastructure.repository.FoodRepository;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQuery;
 import io.github.x4ala1c.tsid.Tsid;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
@@ -22,11 +26,12 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.persistence.EntityManager;
 import jakarta.transaction.Transactional;
-import jakarta.validation.Valid;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
+import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -40,6 +45,11 @@ public final class GetDishesByFood {
 
     @Data
     private static final class GetDishesByFoodRequest extends PaginationRequest {
+
+        private String name;
+        private DishFilter asc;
+        private DishFilter desc;
+        private ActiveStatus status;
 
     }
 
@@ -61,13 +71,13 @@ public final class GetDishesByFood {
                 content = @Content(schema = @Schema(implementation = DishesResponse.class))
         )
         @ApiResponse(
-                description = "Failed getting of the foods.",
+                description = "Failed getting dishes.",
                 responseCode = "400s/500s",
                 content = @Content(schema = @Schema(implementation = WhatEatErrorResponse.class))
         )
         @GetMapping("/dishes/foods/{id}")
-        public ResponseEntity<Object> getDishes(@PathVariable Tsid id, @Valid GetDishesByFoodRequest getDishesRequest) {
-            var response = service.get(getDishesRequest, id);
+        public ResponseEntity<Object> getDishes(@PathVariable Tsid id, @ParameterObject GetDishesByFoodRequest request) {
+            var response = service.get(request, id);
             return ResponseEntity.ok(response);
         }
     }
@@ -83,6 +93,25 @@ public final class GetDishesByFood {
         private final CriteriaBuilderFactory criteriaBuilderFactory;
 
         public DishesResponse get(GetDishesByFoodRequest request, Tsid tsid) {
+            final QDish qDish = QDish.dish;
+            final QRating qRating = QRating.rating;
+            BooleanExpression predicates = qDish.isNotNull();
+
+            if (StringUtils.isNotBlank(request.getName())) {
+                predicates = predicates.and(qDish.name.containsIgnoreCase(request.getName()));
+            }
+
+            if (request.getStatus() != null) {
+                predicates = predicates.and(qDish.status.eq(request.getStatus()));
+            }
+
+            if (request.getAsc() != null && request.getDesc() != null) {
+                throw WhatEatException.builder()
+                        .code(WhatEatErrorCode.WEV_0000)
+                        .reason("filter", "Vui lòng chỉ sử dụng 1 filter: asc hoặc desc.")
+                        .build();
+            }
+
             final WhatEatId WhatEatFoodId = WhatEatId.builder().id(tsid).build();
 
             if (!foodRepository.existsById(WhatEatFoodId)) {
@@ -92,21 +121,50 @@ public final class GetDishesByFood {
                             .build();
             }
 
-            final QDish qDish = QDish.dish;
-            BlazeJPAQuery<Dish> query = new BlazeJPAQuery<>(entityManager, criteriaBuilderFactory);
-            final List<Dish> dishes = query
-                    .select(qDish)
-                    .from(qDish)
-                    .leftJoin(qDish.food).fetchJoin()
-                    .leftJoin(qDish.restaurant).fetchJoin()
-                    .where(qDish.food.id.eq(WhatEatFoodId))
+            predicates = predicates.and(qDish.food.id.eq(WhatEatFoodId));
+
+            JPAQuery<Dish> query = new JPAQuery<>(entityManager);
+            List<Dish> dishes;
+
+            if (DishFilter.REVIEW.equals(request.getAsc()) || DishFilter.REVIEW.equals(request.getDesc())) {
+                 query.select(qDish)
+                        .from(qDish)
+                        .leftJoin(qDish.ratings, qRating)
+                        .having(predicates)
+                        .groupBy(qDish.id);
+
+                if (DishFilter.REVIEW.equals(request.getAsc())) {
+                    query.orderBy(qRating.stars.avg().asc());
+                } else {
+                    query.orderBy(qRating.stars.avg().desc());
+                }
+            } else if (DishFilter.PRICE.equals(request.getAsc()) || DishFilter.PRICE.equals(request.getDesc())) {
+                query.select(qDish)
+                        .from(qDish)
+                        .where(predicates);
+
+                if (DishFilter.PRICE.equals(request.getAsc())) {
+                    query.orderBy(qDish.price.amount.asc());
+                } else {
+                    query.orderBy(qDish.price.amount.desc());
+                }
+            } else {
+                query.select(qDish)
+                        .from(qDish)
+                        .where(predicates);
+            }
+
+            dishes = query
                     .limit(request.getLimit())
                     .offset(request.getOffset())
                     .fetch();
-            final long count = new BlazeJPAQuery<FoodTag>(entityManager, criteriaBuilderFactory)
+
+            final long count = new BlazeJPAQuery<Dish>(entityManager, criteriaBuilderFactory)
                     .select(qDish)
                     .from(qDish)
+                    .where(predicates)
                     .fetchCount();
+
             final DishesResponse response = new DishesResponse(dishes.stream().map(dishMapper::convertToDto).toList(), count);
             response.setLimit(request.getLimit());
             response.setPage(request.getPage());
---
+++ src/main/java/com/exe/whateat/application/dish/entity/DishFilter.java
@@ -0,0 +1,5 @@
+package com.exe.whateat.application.dish.entity;
+
+public enum DishFilter {
+    REVIEW, PRICE
+}
