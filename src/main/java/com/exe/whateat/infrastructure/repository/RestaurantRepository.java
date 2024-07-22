c3c7fa82b87d8d290fd9fbbcfda0d153807f8c5f
HighLink1001 <caolinh1012003@gmail.com>
2024-07-22 12:13:06 +0700

    Add changing in response of post

--- src/main/java/com/exe/whateat/infrastructure/repository/RestaurantRepository.java
+++ src/main/java/com/exe/whateat/infrastructure/repository/RestaurantRepository.java
@@ -23,4 +23,10 @@ public interface RestaurantRepository extends JpaRepository<Restaurant, WhatEatI
                                         @Param("accountEmail") String accountEmail);
 
     boolean existsByNameIgnoreCase(String name);
+
+    @Query("""
+            select r from Restaurant r
+            where r.account.id = :id
+        """)
+    Optional<Restaurant> findByAccountId(WhatEatId id);
 }
